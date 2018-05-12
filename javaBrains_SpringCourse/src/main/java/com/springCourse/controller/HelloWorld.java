9336e66b42948a182a88f23e246271da69ed5a66
SAuraBH <SAuraBH@APPNIT-PC>
2018-05-12 16:57:48 +0530

    First commit

---
+++ javaBrains_SpringCourse/src/main/java/com/springCourse/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.springCourse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld 
+{
+	@RequestMapping("/")
+	public String Home() 
+	{
+		return "Hello Spring Boot";
+	}
+}
